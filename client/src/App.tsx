c0f4feb00e1f5227dbf9ba8c884d5e15c14feb4c
Ritik dangi <ritikdangi445566@gmail.com>
2025-08-28 18:52:32 +0530

    basic react + tailwind css setup is done

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <h1 className='underline color-red-700'>Hello World</h1>
+      <button className='bg-blue-500 text-white p-2 rounded-md'>Click me</button>
+    </>
+  )
+}
+
+export default App
